1bffd600e784c9280c690b5c7735d6985981083d
Jonathan Ralph Wandera <jonathanwandera@gmail.com>
2024-02-12 22:41:25 +0100

    Added basic routing functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-import type { Component } from 'solid-js';
+import type { Component } from "solid-js";
+import { Router, Route } from "@solidjs/router";
+import Home from "./pages/Home";
+import Feed from "./pages/Feed";
+import Profile from "./pages/Profile";
+
+function NavButton() {}
+function NavBar() {
+  return (
+    <div class="fixed bottom-3 bg-sky-100/55 rounded-full w-fit h-fit p-3"></div>
+  );
+}
+
+function AppPage(props) {
+  return (
+    <>
+      {props.children}
+      <NavBar />
+    </>
+  );
+}
 
 const App: Component = () => {
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+    <Router root={AppPage}>
+      <Route path="/home" component={Home}></Route>
+      <Route path="/feed" component={Feed}></Route>
+      <Route path="/prof" component={Profile}></Route>
+      <Route path="/" component={Home}></Route>
+    </Router>
   );
 };
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+import { Component } from "solid-js";
+
+export default function Home(){
+    return <p>Home</p>
+}
