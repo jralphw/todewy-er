35f70dc00961813431836ad073eadddbb5d934f6
Jonathan Ralph Wandera <jonathanwandera@gmail.com>
2024-02-14 23:08:19 +0100

    minor cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { createSignal, type Component, lazy } from "solid-js";
-import { Route, A } from "@solidjs/router";
 import Todo from "./pages/Todo";
 import Profile from "./pages/Profile";
 const Feed = lazy(() => import("./pages/Feed"));
@@ -7,26 +6,28 @@ const Feed = lazy(() => import("./pages/Feed"));
 type navBarRoute = "todo" | "feed" | "prof";
 let [page, setPage] = createSignal<navBarRoute>("todo");
 
-function NavButton({ route }: { route: navBarRoute }) {
-  return (
-    <div
-      class={
-        route === page()
-          ? "rounded-full bg-sky-900 h-8 w-8"
-          : "rounded-full bg-inherit h-8 w-8"
-      }
-      onClick={() => {
-        setPage(route);
-      }}
-    ></div>
-  );
-}
 function NavBar() {
+
+  function NavButton({ path }: { path: navBarRoute }) {
+    return (
+      <div
+        class={
+          path === page()
+            ? "rounded-full bg-sky-900 h-8 w-8"
+            : "rounded-full bg-inherit h-8 w-8"
+        }
+        onClick={() => {
+          setPage(path);
+        }}
+      ></div>
+    );
+  }
+
   return (
     <div class="fixed bottom-3 bg-sky-100/55 rounded-full w-fit h-fit p-2 flex gap-1 self-center">
-      <NavButton route="todo" />
-      <NavButton route="feed" />
-      <NavButton route="prof" />
+      <NavButton path="todo" />
+      <NavButton path="feed" />
+      <NavButton path="prof" />
     </div>
   );
 }
